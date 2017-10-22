afe5e3d3d778ca60fd3262449a09573b1606c507
Travis Ali <ali.gilgil1@gmail.com>
2017-10-21 23:10:55 -0400

    Slighty increase footer size

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -6,17 +6,24 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
 const selected = {
-  textDecoration: 'underline',
   opacity: 0.3,
   pointerEvents: 'none',
   cursor: 'default',
 };
+
+const VerticalAlignMiddleA = styled.a`
+  vertical-align: middle;
+`;
+
+const VerticalAlignMiddleSpan = styled.span`
+  vertical-align: middle;
+`;
 
 class Footer extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -24,11 +31,14 @@ class Footer extends React.PureComponent { // eslint-disable-line react/prefer-s
 
     return (
       <footer className="footer">
-        <div className="left">
-          <a style={(locale === 'fr' ? selected : {})} onClick={() => this.props.changeLanguage('fr')} href="#lang">FR</a>
-          <a style={(locale === 'en' ? selected : {})} onClick={() => this.props.changeLanguage('en')} href="#lang">EN</a>
+        <div className="left" style={{ verticalAlign: 'middle' }}>
+          <VerticalAlignMiddleA style={(locale === 'fr' ? selected : {})} onClick={() => this.props.changeLanguage('fr')} href="#lang">FR</VerticalAlignMiddleA>
+          <VerticalAlignMiddleA style={(locale === 'en' ? selected : {})} onClick={() => this.props.changeLanguage('en')} href="#lang">EN</VerticalAlignMiddleA>
         </div>
-        <div className="right"><FormattedMessage {...messages.lightspeedturtle} /><a className="underline" target="_blank" href="https://www.facebook.com/lightspeedturtle"> Lightspeed Turtle</a></div>
+        <div className="right">
+          <VerticalAlignMiddleSpan><FormattedMessage {...messages.lightspeedturtle} /></VerticalAlignMiddleSpan>&nbsp;
+          <VerticalAlignMiddleA className="underline" target="_blank" href="https://www.facebook.com/lightspeedturtle">Lightspeed Turtle</VerticalAlignMiddleA>
+        </div>
       </footer>
     );
   }
